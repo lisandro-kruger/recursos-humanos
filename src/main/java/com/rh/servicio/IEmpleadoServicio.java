ee7ec0c9ecc435020fa89ee820606aceebd77bd4
lisandro-kruger <lisandro.kruger@outlook.es>
2023-09-12 11:29:39 -0300

    20230912

---
+++ src/main/java/com/rh/servicio/IEmpleadoServicio.java
@@ -0,0 +1,15 @@
+package com.rh.servicio;
+
+import com.rh.modelo.Empleado;
+import java.util.List;
+
+public interface IEmpleadoServicio {
+    public List<Empleado> listarEmpleados();
+
+    public Empleado buscarEmpleadoPorId(Integer idEmpleado);
+
+    public Empleado guardarEmpleado(Empleado empleado);
+
+    public void eliminarEmpleado(Empleado empleado);
+
+}
---
+++ src/test/java/com/recursosHumanos/RecursosHumanosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recursosHumanos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecursosHumanosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
